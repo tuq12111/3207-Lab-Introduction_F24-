a1efd7146ba92973305eaf87e7d3bce24c61d442
John Currie <tuq12111@temple.edu>
2024-08-30 11:12:00 -0400

    main func linked

--- print_random.c
+++ print_random.c
@@ -1,4 +1,13 @@
 //Given Project code
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "random.h"
+
+char ranchar();
+char* rand_string_alloc(size_t size);
+char* rand_string(char *s, size_t size);
+
 char* rand_string_alloc(size_t size)
 {
      char *s = malloc(size + 1);
@@ -6,4 +15,23 @@ char* rand_string_alloc(size_t size)
          rand_string(s, size);
      }
      return s;
+}
+
+char* rand_string(char *s, size_t size)
+{
+    while (size-- > 0) {
+        *s++ = ranchar();
+    }
+    *s = '\0';
+    return s;
+}
+
+int main()
+{
+    srand(time(NULL));
+
+    char *s = rand_string_alloc(7);
+    printf("%s\n", s);
+    free(s);
+    return 0;
 }
